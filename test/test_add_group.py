3dd044273f53963edd5c2f3fe7f91c5be08bd93d
AlexEkinin <kiluy9iuoh@yandex.ru>
2017-02-25 09:48:08 -0800

    2.3

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -13,10 +13,10 @@ def app(request):
 def test_add_group(app):  # self - obekt, v kotorom vizivaetsa metod
     app.session.login(username="admin", password="secret")
     # sozdaem obekt tipa Group i peredaem perametri v ego konstruktor
-    app.create_group(Group(name="fdgsgh", header="dsfgsdfg", footer="xcbcvx"))
+    app.group.create(Group(name="fdgsgh", header="dsfgsdfg", footer="xcbcvx"))
     app.session.logout()
 
 def test_add_empty_group(app):  # self - obekt, v kotorom vizivaetsa metod
     app.session.login(username="admin", password="secret")
-    app.create_group(Group(name="", header="", footer=""))
+    app.group.create(Group(name="", header="", footer=""))
     app.session.logout()
